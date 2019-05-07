d799ec2529f1230a57a16e6cc427b8ed6cc3b000
Anatol <yarmolovich1986@gmail.com>
2019-05-07 12:50:01 +0300

    last version, add zoom

--- PhotoResizer/ViewController.swift
+++ PhotoResizer/ViewController.swift
@@ -1,8 +1,4 @@
-//
-//  ViewController.swift
-//  PhotoResizer
-//
-//  Created by Anatol Uarmolovich on 5/7/19.
+
 //  Copyright © 2019 Anatol Uarmolovich. All rights reserved.
 //
 
@@ -10,11 +6,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageForRotate: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let zoomGesture = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.handleScaleChange(sender:)))
+        let rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(ViewController.handleRotation(sender:)))
+       self.imageForRotate.addGestureRecognizer(rotationGesture)
+        self.imageForRotate.addGestureRecognizer(zoomGesture)
     }
-
+    @objc func handleRotation(sender: UIRotationGestureRecognizer) {
+        guard sender.view != nil else {return}
+        
+        if sender.state == .began || sender.state == .changed {
+            sender.view?.transform = sender.view!.transform.rotated(by: sender.rotation)
+            sender.rotation = 0
+        }
+    }
+    @objc func handleScaleChange(sender: UIPinchGestureRecognizer) {
+        guard sender.view != nil else {
+            return
+        }
+        if sender.state == .began || sender.state == .changed {
+            sender.view?.transform = sender.view!.transform.scaledBy(x: sender.scale, y: sender.scale)
+            sender.scale = 1.0
+        }
+    }
 
 }
 
